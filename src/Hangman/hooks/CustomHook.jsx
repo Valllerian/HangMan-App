857453a270dcc6771e39ff8354d4859e922a41f4
Valerii Bihun <valeriibihun.co@gmail.com>
2022-04-30 23:25:31 -0600

    code refactored

--- src/Hangman/hooks/CustomHook.jsx
+++ src/Hangman/hooks/CustomHook.jsx
@@ -8,12 +8,3 @@ const CustomHook = () => {
 
 export default CustomHook
 
-import React from 'react'
-
-const CustomHook = () => {
-  return (
-    <div>CustomHook</div>
-  )
-}
-
-export default CustomHook
